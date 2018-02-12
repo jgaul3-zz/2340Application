cd75aac27aca6f6ab70b6371f2a69cd9f4af830b
JGaul <jgaul3@gmail.com>
2018-02-11 21:29:51 -0500

    Introductory commit - Jon Gaul

---
+++ app/src/main/java/edu/gatech/cs2340/cs2340application/controllers/OpeningScreen.java
@@ -0,0 +1,21 @@
+package edu.gatech.cs2340.cs2340application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/** Top level window.
+ *
+ * Directs the user to either the login screen or the guest screen.
+ *
+ */
+public class OpeningScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+
+
+    }
+}
