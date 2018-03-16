4ea280e7535cb71462d2f2b9e026d37b52853547
JordanE655 <isakaru12@yahoo.com>
2018-03-16 03:16:45 -0400

    ShelterDetail Movement

--- app/src/main/java/edu/gatech/cs2340/cs2340application/controllers/AppScreen.java
+++ app/src/main/java/edu/gatech/cs2340/cs2340application/controllers/AppScreen.java
@@ -2,11 +2,8 @@ package edu.gatech.cs2340.cs2340application.controllers;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,7 +12,6 @@ import android.widget.ListView;
 
 
 import edu.gatech.cs2340.cs2340application.R;
-import edu.gatech.cs2340.cs2340application.ShelterDetailActivity;
 import edu.gatech.cs2340.cs2340application.model.*;
 
 public class AppScreen extends AppCompatActivity {
